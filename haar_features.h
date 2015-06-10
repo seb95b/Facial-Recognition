f87fb9bf985a390bf9483c8fd1171670937abfa4
seb95b <sebastienbrunat@hotmail.fr>
2015-06-11 01:25:38 +0200

    haar_features.h updated

---
+++ haar_features.h
@@ -0,0 +1,23 @@
+#ifndef HAAR_FEATURES_H
+#define HAAR_FEATURES_H
+
+struct features {
+	size_t x, y, scale_x, scale_y;
+	int type, val;
+};
+
+int row_sum(int x, int y, SDL_Surface *img);
+
+int compute_int_image_rec(int x, int y, unsigned int **int_image, SDL_Surface *img);
+
+void compute_int_image(unsigned int **int_image, SDL_Surface *img);
+
+static int get_int_image(int x, int y, unsigned int **int_image);
+
+static int compute_features(int x, int y, int type, int scale_x, int scale_y , unsigned int **int_image);
+
+struct features *compute_f(unsigned int **int_image);
+
+int compute_size(int frameSize);
+
+#endif
